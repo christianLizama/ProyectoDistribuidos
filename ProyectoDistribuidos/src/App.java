98d2029386553962223528f164bcc21af2b06ab5
ChristianLizama <ckill661@gmail.com>
2022-09-10 23:30:41 -0300

    clase hilo

--- ProyectoDistribuidos/src/App.java
+++ ProyectoDistribuidos/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Hilo hilo = new Hilo();
+        hilo.hola();
     }
 }
