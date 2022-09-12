ee31a2879157d0e25c5e5c073df2ca292ee28160
ChristianLizama <ckill661@gmail.com>
2022-09-12 00:22:00 -0300

    Sin importaciones innecesarias

--- ProyectoDistribuidos/src/App.java
+++ ProyectoDistribuidos/src/App.java
@@ -1,14 +1,10 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
-
-import javax.swing.text.StyledEditorKit.BoldAction;
 
 public class App {
 
